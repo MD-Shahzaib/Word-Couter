ad3f9a307e9412030040465231d264a6e820921b
Muhammad Shahzaib <mdshahzaib36@gmail.com>
2023-07-07 19:16:20 -0700

    Project setup and complete requirement in future add more features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [text, setText] = useState('');
+  const wordCount = text.trim().split(/\s+/).filter(word => word !== '').length;
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Word Counter App</h1>
+      <textarea
+        className="border border-gray-300 p-2 mb-4 w-full"
+        rows="5"
+        placeholder="Type or paste your text here"
+        value={text}
+        onChange={handleChange}
+      ></textarea>
+      <p className="mb-4">Word Count: {wordCount}</p>
     </div>
   );
-}
+};
 
 export default App;
